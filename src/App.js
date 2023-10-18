dc194e80a8b7d5980ed57a97965854420724037d
Poojan Patel <ppatel2099@gmail.com>
2023-10-18 09:42:05 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import products from "./db/data";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import Card from "./components/Card";
+import "./index.css";
+import { useState } from "react";
 
 function App() {
+  const [selectedCategory, setSelectedCategory]=useState(null);
+
+  //input query
+  const [query, setQuery]=useState("");
+
+  const handleInputChange=event=>{
+    setQuery(event.target.value);
+  }
+
+  const filteredItems=products.filter(product=>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase())!== -1
+  );
+
+  //radio filter
+  const handleChange=event=>{
+    setSelectedCategory(event.target.value);
+  }
+
+  //buttons filter
+  const handleClick=event=>{
+    setSelectedCategory(event.target.value);
+  }
+
+  function filteredData(products, selected, query){
+    let filteredProducts=products
+
+    //filtering input items
+    if(query){
+      filteredProducts=filteredItems
+    }
+
+    //selected  filter
+    if(selected){
+      filteredProducts=filteredProducts.filter(({category,color,company, newPrice, title})=> 
+      category===selected || color===selected || company ===selected || newPrice===selected || title===selected) 
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
   );
 }
 
